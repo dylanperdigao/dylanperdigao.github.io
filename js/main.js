9087f1d044bc036408a69c933198946f1b975e5d
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-01-26 18:44:12 +0000

    fix

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ function main(){
 	var h = 1000;
 	ctx.fillStyle = "green"
 	ctx.beginPath();
-		ctx.lineTo(w*2/3,0,w,0);
+		ctx.lineTo(w/3,0,w,0);
 		ctx.lineTo(w,0,w,h);
 		ctx.lineTo(w,h,w/2,h);
-		ctx.lineTo(w/2,h,w*2/3,0);
+		ctx.lineTo(w*2/3,h,w/3,0);
 	ctx.stroke();
 	ctx.fill();
 }
