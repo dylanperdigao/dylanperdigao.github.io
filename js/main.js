dac22d2090bfa9bed9fa2e734582aa9a56925775
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-07-19 21:52:57 +0100

    prog languages

--- js/main.js
+++ js/main.js
@@ -1,58 +1,66 @@
 "use strict";
-(function(){var e,t,a,d,r,l,i,m,n,u,o;for(window.CREDLY_EMBED_JS_LOADER_VERSION="20210331",e="www.credly.com",u=function(e){if(null!=e)return/(.*\.credly.com$|(acclaim\.local|localhost|web):500[0-1]$)/.test(e)?e:void 0},l=0,i=(m=function(e){var t,a,d,r,l;if(null!=document.querySelectorAll)return document.querySelectorAll("[data-"+e+"]");for(l=[],d=0,r=(t=document.getElementsByTagName("*")).length;d<r;d++)(a=t[d]).getAttribute("data-"+e)&&l.push(a);return l}("share-badge-id")).length;l<i;l++)a=(d=m[l]).attributes.getNamedItem("data-share-badge-id").value,o=d.attributes.getNamedItem("data-iframe-width").value,r=d.attributes.getNamedItem("data-iframe-height").value,(t=u(null!=(n=d.attributes.getNamedItem("data-badge-host"))?n.value:void 0))||(t=e),d.outerHTML='<iframe name="acclaim-badge" allowTransparency="true" frameborder="0" id="embedded-badge-'+a+'" scrolling="no" src="//'+t+"/embedded_badge/"+a+'" style="width: '+o+"px; height: "+r+'px;" title="View my verified achievement on Credly." ></iframe>'}).call(this);
+
+let lang = null;
+
 function main(){
-	var vertices;
-	var canvas = document.getElementsByTagName("canvas");
-	for(var i=0;i<canvas.length;i++){
-		let ctx = canvas[i].getContext('2d');
-		let w = ctx.canvas.width;
-		let h = ctx.canvas.height;
-		switch (i){
-			case 0:
-				vertices = new Array(
-					new Vertex(w/3,0),
-					new Vertex(w,0),
-					new Vertex(w,h),
-					new Vertex(w*2/3,h),
-				);
-				break;
-			case 1:
-				vertices = new Array(
-					new Vertex(w*2/3,0),
-					new Vertex(w,0),
-					new Vertex(w,h),
-					new Vertex(w*9/10,h),
-				);
-				break;
-			case 2:
-				vertices = new Array(
-					new Vertex(w*9/10,0),
-					new Vertex(w,0),
-					new Vertex(w,h),
-					new Vertex(w*9/10,h),
-				);
-				break;
-			case 3:
-				vertices = new Array(
-					new Vertex(w*9/10,0),
-					new Vertex(w,0),
-					new Vertex(w,h),
-					new Vertex(w*2/3,h),
-				);
-				break;
-			case 4:
-				vertices = new Array(
-					new Vertex(w*2/3,0),
-					new Vertex(w,0),
-					new Vertex(w,h),
-					new Vertex(w/3,h),
-				);
-				break;
-		}
-		let background = new Background(new Array(new Polygon(vertices,"#06D6A0")));
-		background.draw(ctx);
-	}
+	loadJSON('lang/EN.json', getLang)
+	loadJSON('data/languages.json',setLanguages);
+	loadJSON('data/programmingLanguages.json',setSkills);
 }
 
+function loadJSON(url, func){
+	const req = new XMLHttpRequest();
+	req.open('GET', url);
+	req.onload = function () {
+		func(this);
+	};
+	req.send();
+}
+
+function getLang(req){
+	lang = JSON.parse(req.responseText);
+}
+
+function setLanguages(req){
+	let languages = JSON.parse(req.responseText);
+	for (let i=0;i<languages.length;i++){
+		document.getElementById("languages").innerHTML +=
+			"<div class='skillLine'>"+
+			"<div class='col'>"+
+			"<img class='skill' src='icons/languages/"+languages[i][0]+".png'>" +
+			"<label><orange>'"+lang[languages[i][0]]+"'</orange> :</label>" +
+			"</div>"+
+			"<div class='col'>"+
+			"<progress value='"+languages[i][1]+"'></progress>" +
+			"<label><green>"+languages[i][1]+"</green>,</label>"+
+			"</div>"+
+			"</div>"
+		;
+	}
+}
+function setSkills(req){
+	let languages = JSON.parse(req.responseText);
+	languages = Object.keys(languages).map(function(key) {
+		return [key, languages[key]];
+	});
+	languages.sort(function(first, second) {
+		return second[1] - first[1];
+	});
+	
+	for (let i=0;i<languages.length;i++){
+		document.getElementById("programmingLanguages").innerHTML +=
+			"<div class='skillLine'>"+
+				"<div class='col'>"+
+					"<img class='skill' src='icons/programing_languages/"+languages[i][0].toLowerCase()+".png'>" +
+					"<label><orange>'"+languages[i][0]+"'</orange> :</label>" +
+				"</div>"+
+				"<div class='col'>"+
+					"<progress value='"+languages[i][1]/100+"'></progress>" +
+					"<label><green>"+languages[i][1]+"%</green>,</label>"+
+				"</div>"+
+			"</div>"
+		;
+	}
+}
 
 
