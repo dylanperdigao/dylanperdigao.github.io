dbfedbb3924adc29e16734da84d89d15d90384de
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-07-22 23:25:11 +0100

    animations

--- js/animation.js
+++ js/animation.js
@@ -1,44 +1,20 @@
-"use strict";
+function sectionAnimation() {
+	// Remove the transition class
+	const squares = document.querySelectorAll('.square');
+	const squares_wrappers = document.querySelectorAll('.square-wrapper');
+	for (let i = 0; i < squares.length; i++) {
+		squares[i].classList.remove('square-transition');
+		// Create the observer, same as before:
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+						squares[i].classList.add('square-transition');
+					return;
+				}
+				squares[i].classList.remove('square-transition');
+			});
+		});
+		observer.observe(squares_wrappers[i]);
+	}
 
-function animation(){
-	var canvas = document.getElementById("c2");
-	var ctx = canvas.getContext('2d');
-	var r=0;
-	var y = ctx.canvas.height;
-	var interval=1;
-	var alpha=0;
-	var n = 15;
-	var circles = new Array();
-	for(var i=0;i<n;i++){
-		circles.push(new Circle(500,500,5,"white"));
-		circles[i].move(circles[i].x0,y)
-	}
-	//animation
-	var anim = function(){
-		ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-		for(var i=0;i<n;i++){
-			if(y>ctx.canvas.height/2){
-				y-=interval/5;
-				circles[i].move(circles[i].x,y);
-			}else{
-				alpha = (i/n)*2*Math.PI;
-				circles[i].move(circles[i].x0+r*Math.cos(alpha),circles[i].y0+r*Math.sin(alpha));
-			}
-			circles[i].draw(ctx);
-		}
-		if(y<ctx.canvas.height/2){
-			r+=interval;
-		}
-		if(r/4>ctx.canvas.height){
-			r=0;
-			y=ctx.canvas.height;
-			for(var i=0;i<n;i++){
-				circles[i].move(circles[i].x0,y);
-			}
-		}
-		aboutBackground(ctx);
-		setTimeout(anim,interval);
-	}
-	setTimeout(anim,interval);
-	
 }
